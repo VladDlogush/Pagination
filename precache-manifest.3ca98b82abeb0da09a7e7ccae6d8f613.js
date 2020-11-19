e4807e8fd9af72a179a31c47de34d4838de15ca2
VladDlogush <vlad.dlogush21@gmail.com>
2020-11-19 22:31:44 +0300

    Updates

---
+++ precache-manifest.3ca98b82abeb0da09a7e7ccae6d8f613.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e42a13bf461ede2880ea",
+    "url": "/VladDlogush/Pagination/static/js/main.a0c4e0bc.chunk.js"
+  },
+  {
+    "revision": "5209ed6f83d5a18df096",
+    "url": "/VladDlogush/Pagination/static/js/runtime~main.dd911f19.js"
+  },
+  {
+    "revision": "d5d19684364d4c85bd4e",
+    "url": "/VladDlogush/Pagination/static/js/2.961606d0.chunk.js"
+  },
+  {
+    "revision": "2303854f20fb68ff500233f9f416fc9c",
+    "url": "/VladDlogush/Pagination/index.html"
+  }
+];
